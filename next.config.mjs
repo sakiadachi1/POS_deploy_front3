ceb55e7f7c8b7bd1636dce0be97ea15b366482b9
saki <sakiadachi1@gmail.com>
2025-02-10 16:15:06 +0900

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://tech0-gen8-step4-pos-app-34.azurewebsites.net",
+  },
+  //output: "standalone",
 };
 
 export default nextConfig;
